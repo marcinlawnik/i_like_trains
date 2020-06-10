dd57fa1561f74acc0bda1e8f866a728d6efa93aa
Robert Banaszak <robert.banaszak1997@gmail.com>
2020-06-10 08:12:19 +0200

    minor positioning tweaks

--- lib/i_like_trains/map_config.ex
+++ lib/i_like_trains/map_config.ex
@@ -10,25 +10,25 @@ defmodule ILikeTrains.MapConfig do
   #Places top-to-bottom, left-to-right
   @places_data %{
     "Lincoln Center" => %{name: "Lincoln Center", x: 75, y: 30},
-    "Central Park" => %{name: "Central Park", x: 280, y: 25},
-    "Midtown West" => %{name: "Midtown West", x: 60, y: 145},
+    "Central Park" => %{name: "Central Park", x: 280, y: 30},
+    "Midtown West" => %{name: "Midtown West", x: 75, y: 145},
     "Times Square" => %{name: "Times Square", x: 205, y: 95},
-    "United Nations" => %{name: "United Nations", x: 380, y: 120},
-    "Empire State" => %{name: "Empire State", x: 280, y: 190},# Building
-    "Chelsea" => %{name: "Chelsea", x: 85, y: 260},
-    "Gramercy Park" => %{name: "Gramercy Park", x: 525, y: 250},
+    "United Nations" => %{name: "United Nations", x: 380, y: 95},
+    "Empire State" => %{name: "Empire State", x: 320, y: 190},# Building
+    "Chelsea" => %{name: "Chelsea", x: 75, y: 260},
+    "Gramercy Park" => %{name: "Gramercy Park", x: 550, y: 260},
     "Greenwich Village" => %{name: "Greenwich Village", x: 305, y: 355},
-    "East Village" => %{name: "East Village", x: 515, y: 350},
+    "East Village" => %{name: "East Village", x: 550, y: 355},
     "Soho" => %{name: "Soho", x: 140, y: 450},
-    "Chinatown" => %{name: "Chinatown", x: 330, y: 460},
+    "Chinatown" => %{name: "Chinatown", x: 305, y: 460},
     "Lower East Side" => %{name: "Lower East Side", x: 550, y: 460},
-    "Wall Street" => %{name: "Wall Street", x: 300, y: 600},
+    "Wall Street" => %{name: "Wall Street", x: 305, y: 600},
     "Brooklyn" => %{name: "Brooklyn", x: 455, y: 600},
   }
 
   @routes_data [
     # let's keep places in alphabetical order - it will be easier to pattern match them
-    %{id: 1, places: ["Lincoln Center", "Central Park"], color: "orange", cost: 2},
+    %{id: 1, places: ["Central Park", "Lincoln Center"], color: "orange", cost: 2},
     %{id: 2, places: ["Lincoln Center", "Times Square"], color: "green", cost: 2},
     %{id: 3, places: ["Lincoln Center", "Times Square"], color: "blue", cost: 2, position_shift: "bottom-left"},
     %{id: 4, places: ["Lincoln Center", "Midtown West"], color: "red", cost: 2},
@@ -37,19 +37,19 @@ defmodule ILikeTrains.MapConfig do
     %{id: 6, places: ["Central Park", "Times Square"], color: "red", cost: 2, position_shift: "top-left"},
     %{id: 7, places: ["Central Park", "United Nations"], color: "pink", cost: 3},
 
-    %{id: 8, places: ["Midtown West", "Times Square"], color: "red", cost: 1},#no color
-    %{id: 9, places: ["Midtown West", "Empire State"], color: "green", cost: 2},
-    %{id: 10, places: ["Midtown West", "Chelsea"], color: "blue", cost: 2},
+    %{id: 8, places: ["Midtown West", "Times Square"], color: "red", cost: 1}, # no color
+    %{id: 9, places: ["Empire State", "Midtown West"], color: "green", cost: 2},
+    %{id: 10, places: ["Chelsea", "Midtown West"], color: "blue", cost: 2},
 
-    %{id: 11, places: ["Times Square", "United Nations"], color: "red", cost: 2},#no color
-    %{id: 12, places: ["Times Square", "Empire State"], color: "pink", cost: 1},
-    %{id: 13, places: ["Times Square", "Empire State"], color: "orange", cost: 1, position_shift: "bottom-right"},
+    %{id: 11, places: ["Times Square", "United Nations"], color: "red", cost: 2}, # no color
+    %{id: 12, places: ["Empire State", "Times Square"], color: "pink", cost: 1, position_shift: "left"},
+    %{id: 13, places: ["Empire State", "Times Square"], color: "orange", cost: 1, position_shift: "right"},
 
-    %{id: 14, places: ["United Nations", "Empire State"], color: "black", cost: 2},
-    %{id: 15, places: ["United Nations", "Gramercy Park"], color: "green", cost: 3},
+    %{id: 14, places: ["Empire State", "United Nations"], color: "black", cost: 2},
+    %{id: 15, places: ["Gramercy Park", "United Nations"], color: "green", cost: 3},
 
-    %{id: 16, places: ["Chelsea", "Empire State"], color: "green", cost: 2},#no color
-    %{id: 17, places: ["Chelsea", "Empire State"], color: "red", cost: 2, position_shift: "bottom"},#no color
+    %{id: 16, places: ["Chelsea", "Empire State"], color: "green", cost: 2}, # no color
+    %{id: 17, places: ["Chelsea", "Empire State"], color: "red", cost: 2, position_shift: "bottom"}, # no color
     %{id: 18, places: ["Chelsea", "Gramercy Park"], color: "orange", cost: 2},
     %{id: 19, places: ["Chelsea", "Greenwich Village"], color: "red", cost: 3},
     %{id: 20, places: ["Chelsea", "Greenwich Village"], color: "green", cost: 3, position_shift: "bottom-left"},
@@ -57,30 +57,30 @@ defmodule ILikeTrains.MapConfig do
 
     %{id: 22, places: ["Gramercy Park", "Greenwich Village"], color: "black", cost: 2},
     %{id: 23, places: ["Gramercy Park", "Greenwich Village"], color: "pink", cost: 2, position_shift: "bottom-right"},
-    %{id: 24, places: ["Gramercy Park", "East Village"], color: "red", cost: 1},#no color
+    %{id: 24, places: ["East Village", "Gramercy Park"], color: "red", cost: 1}, # no color
 
     %{id: 25, places: ["Greenwich Village", "Soho"], color: "orange", cost: 2},
-    %{id: 26, places: ["Greenwich Village", "Chinatown"], color: "black", cost: 2},#no color
-    %{id: 27, places: ["Greenwich Village", "Chinatown"], color: "black", cost: 2, position_shift: "bottom-right"},#no color
-    %{id: 28, places: ["Greenwich Village", "Lower East Side"], color: "black", cost: 2},#no color
-    %{id: 29, places: ["Greenwich Village", "East Village"], color: "blue", cost: 2},
+    %{id: 26, places: ["Chinatown", "Greenwich Village"], color: "black", cost: 2, position_shift: "left"}, # no color
+    %{id: 27, places: ["Chinatown", "Greenwich Village"], color: "black", cost: 2, position_shift: "right"}, # no color
+    %{id: 28, places: ["Greenwich Village", "Lower East Side"], color: "black", cost: 2}, # no color
+    %{id: 29, places: ["East Village", "Greenwich Village"], color: "blue", cost: 2},
 
-    %{id: 30, places: ["Soho", "Wall Street"], color: "blue", cost: 2},#no color
+    %{id: 30, places: ["Soho", "Wall Street"], color: "blue", cost: 2}, # no color
 
-    %{id: 31, places: ["Chinatown", "Wall Street"], color: "green", cost: 1},
-    %{id: 32, places: ["Chinatown", "Wall Street"], color: "pink", cost: 1, position_shift: "bottom-right"},
-    %{id: 33, places: ["Chinatown", "Brooklyn"], color: "orange", cost: 3},
-    %{id: 34, places: ["Chinatown", "Brooklyn"], color: "red", cost: 3, position_shift: "top-right"},
+    %{id: 31, places: ["Chinatown", "Wall Street"], color: "green", cost: 1, position_shift: "left"},
+    %{id: 32, places: ["Chinatown", "Wall Street"], color: "pink", cost: 1, position_shift: "right"},
+    %{id: 33, places: ["Brooklyn", "Chinatown"], color: "orange", cost: 3},
+    %{id: 34, places: ["Brooklyn", "Chinatown"], color: "red", cost: 3, position_shift: "top-right"},
     %{id: 35, places: ["Chinatown", "Lower East Side"], color: "blue", cost: 1},
 
-    %{id: 36, places: ["Lower East Side", "Brooklyn"], color: "blue", cost: 3}, #nocolor
-    %{id: 36, places: ["Lower East Side", "East Village"], color: "black", cost: 1},
+    %{id: 36, places: ["Brooklyn", "Lower East Side"], color: "blue", cost: 3}, #nocolor
+    %{id: 37, places: ["East Village", "Lower East Side"], color: "black", cost: 1},
 
-    %{id: 37, places: ["Wall Street", "Brooklyn"], color: "blue", cost: 3},
-    %{id: 38, places: ["Wall Street", "Brooklyn"], color: "black", cost: 3, position_shift: "bottom"},
+    %{id: 38, places: ["Brooklyn", "Wall Street"], color: "blue", cost: 3},
+    %{id: 39, places: ["Brooklyn", "Wall Street"], color: "black", cost: 3, position_shift: "bottom"},
 
-    %{id: 39, places: ["Gramercy Park", "Empire State"], color: "red", cost: 1},
-    %{id: 40, places: ["Gramercy Park", "Empire State"], color: "blue", cost: 1, position_shift: "top-left"},
+    %{id: 40, places: ["Empire State", "Gramercy Park"], color: "red", cost: 1, position_shift: "bottom"},
+    %{id: 41, places: ["Empire State", "Gramercy Park"], color: "blue", cost: 1, position_shift: "top"},
   ]
 
   @ticket_data [
